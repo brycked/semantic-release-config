eea1fa4eb41e3e5bce7d30bb93f5ca3fb8300b45
Brycked <brycked@proton.me>
2023-03-29 15:42:48 +0200

    feat(package): export semantic-release config

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+const config: import("semantic-release").Options = {
+  branches: [
+    "main",
+    {
+      name: "beta",
+      prerelease: true,
+    },
+  ],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "CHANGELOG.md",
+      },
+    ],
+    "@semantic-release/npm",
+    "@semantic-release/github",
+    [
+      "@semantic-release/git",
+      {
+        assets: "CHANGELOG.md",
+      },
+    ],
+  ],
+};
+module.exports = config;
